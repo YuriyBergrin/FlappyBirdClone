fdf3ce89f6416056d6da6575eb8aafd2b349c3f2
YuriyBergrin <bergrin1991@gmail.com>
2020-09-05 19:33:28 +0300

    add bird Circle

--- core/src/com/gmail/bergrin/FlappyBird.java
+++ core/src/com/gmail/bergrin/FlappyBird.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Circle;
 
 import java.util.Random;
 
@@ -31,6 +32,8 @@ public class FlappyBird extends ApplicationAdapter {
     float tubeX[] = new float[tubeNumber];
     float tubeShift[] = new float[tubeNumber];
     float distanceBetweenTubes;
+
+    Circle birdCircle;
 
     @Override
     public void create() {
